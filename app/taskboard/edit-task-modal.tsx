7cad937f225488341b567d3b73f290eaa677f91a
SourC <souriya.khaosanga@gmail.com>
2024-09-28 00:03:38 -0700

    modal

--- app/taskboard/edit-task-modal.tsx
+++ app/taskboard/edit-task-modal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/Dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
