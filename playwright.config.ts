32ca8099bd4f2eafd28e30653b87317dafa6c2f1
SourC <souriya.khaosanga@gmail.com>
2024-10-03 12:44:30 -0700

    update our e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,13 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'http://localhost:3000', // Base URL for your application
+    baseURL: process.env.BASE_URL, // Use the deployment URL
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true, // Run tests in headless mode
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET || '',
+    },
   },
 
   /* Configure projects for major browsers */
