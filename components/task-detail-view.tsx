e9f001c38ed9cd7926f999ffefaf49d3b326e0a6
SourC <souriya.khaosanga@gmail.com>
2024-10-05 07:04:15 -0700

    chore: Refactor project structure and add new features

--- components/task-detail-view.tsx
+++ components/task-detail-view.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { Task, Agent, TaskDetailViewProps } from '@/types';
+import {  TaskDetailViewProps } from '@/types';
 
 
 
