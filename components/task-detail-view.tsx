e680dca5f49f366a086670932fa4ecb248250b71
SourC <souriya.khaosanga@gmail.com>
2024-10-05 06:54:17 -0700

    works with nested routes

--- components/task-detail-view.tsx
+++ components/task-detail-view.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import {  TaskDetailViewProps } from '@/types';
+import { Task, Agent, TaskDetailViewProps } from '@/types';
 
 
 
