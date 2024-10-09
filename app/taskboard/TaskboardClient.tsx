fa7361795033137e9e99365a38e182d0d46722d9
SourC <souriya.khaosanga@gmail.com>
2024-10-08 22:49:06 -0700

    chore: Update npm dependencies and add ethers library

---
+++ app/taskboard/TaskboardClient.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+export default function TaskboardClient() {
+  // Your existing taskboard logic here
+  return (
+    <div>
+      {/* Your taskboard JSX */}
+    </div>
+  );
+}
